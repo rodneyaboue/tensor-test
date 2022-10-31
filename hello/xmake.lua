a57efcf310e2c3244e36098c5fb76418d4c02189
Rodney Aboue <62360031+rodneyAboue@users.noreply.github.com>
2022-10-31 17:21:07 +0100

    Add files via upload

--- hello/xmake.lua
+++ hello/xmake.lua
@@ -2,8 +2,8 @@ add_rules("mode.debug", "mode.release")
 
 option("tensorflow")
 set_showmenu(true)
-add_includedirs("$(tensorflow)/include", {public = true})
-add_linkdirs("$(tensorflow)/lib")
+add_includedirs("C:/Users/kenneth_aboue/Documents/tensorflow_test/include", {public = true})
+add_linkdirs("C:/Users/kenneth_aboue/Documents/tensorflow_test/lib")
 add_links("tensorflow")
 option_end()
 
