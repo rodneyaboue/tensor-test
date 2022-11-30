2386b76d91b7c0c7632ff98491cde1f4b2c35cd8
rodneyaboue <62360031+rodneyaboue@users.noreply.github.com>
2022-11-30 11:27:36 +0100

    Update xmake.lua

--- hello/xmake.lua
+++ hello/xmake.lua
@@ -1,13 +1,4 @@
 add_rules("mode.debug", "mode.release")
-
-option("tensorflow")
-set_showmenu(true)
-add_includedirs("C:/Users/kenneth_aboue/Documents/tensorflow_test/include", {public = true})
-add_linkdirs("C:/Users/kenneth_aboue/Documents/tensorflow_test/lib")
-add_links("tensorflow")
-option_end()
-
 target("hello")
     set_kind("binary")
     add_files("src/*.c")
-    add_options("tensorflow")
