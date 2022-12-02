d2739340b71935a9ad30c06f68b8c1055b2d5501
rodneyaboue <62360031+rodneyaboue@users.noreply.github.com>
2022-12-02 17:47:38 +0100

    Update main.c

--- hello/src/main.c
+++ hello/src/main.c
@@ -2,6 +2,6 @@
 #include <tensorflow/c/c_api.h>
 
 int main() {
-  printf("Hello from TensorFlow C library version %s\n", TF_Version());
+  printf("Hello from TfyhrhrhrhensorFlow C library version %s\n", TF_Version());
   return 0;
 }
